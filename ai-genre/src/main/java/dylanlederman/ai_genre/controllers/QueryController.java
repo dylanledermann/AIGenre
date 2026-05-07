a6c3715f0163d18b53978a8fe4c74bba2ef50217
dylhead <dylhead2003@gmail.com>
2026-05-07 12:56:20 -0700

    Removed unused import.

--- ai-genre/src/main/java/dylanlederman/ai_genre/controllers/QueryController.java
+++ ai-genre/src/main/java/dylanlederman/ai_genre/controllers/QueryController.java
@@ -4,7 +4,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
